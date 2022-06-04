0acb5e073f679d18d8769a343bfa4792d7cc4291
yayner <yayner2002@gmail.com>
2022-06-04 14:44:29 +0300

    Add action dispatch reducer

--- src/index.js
+++ src/index.js
@@ -2,7 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+
+// action 
+
+const inc = () => {
+  return{
+    type:'INCREMENT'
+  }
+}
+//   const dec = () => {
+//     return{
+//       type:'DECREMENT'
+//     }
+// }
+//reducer
+
+const counter = (state = 0, action) => {
+  switch(action.type){
+    case 'INCREMENT':
+      return state + 1;
+      case 'DECREMENT':
+        return state - 1;
+        default: return state
+  }
+
+
+}
+//dispatch
+let store = configureStore(counter)
+store.dispatch(inc())
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,7 +41,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
